490548be41d62bfb83e643896b662a05edb35c06
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-08-27 01:00:31 +0900

    init

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 6.49.2"
+    }
+  }
+  required_version = ">= 1.13.0"
+}
+
+provider "google" {
+  region = "asia-northeast1"
+}
+
+
