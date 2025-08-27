c4a6a28387b33ca28f6de25014169f99dff65036
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-08-27 11:24:55 +0900

    added new compute engine

--- main.tf
+++ main.tf
@@ -9,7 +9,31 @@ terraform {
 }
 
 provider "google" {
+  project = var.project_id
   region = "asia-northeast1"
 }
 
+
+resource "google_compute_instance" "vm" {
+  name         = "gce-tokyo"
+  machine_type = "e2-micro"
+  zone         = "asia-northeast1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
+
+
+variable "project_id" {
+  description = "GCP project ID to deploy resources into"
+  type        = string
+}
 
